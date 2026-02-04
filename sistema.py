83c0d3431ca099bde0bd5399c66aaa317e99564c
João Costa <PedroM_187@hotmail.com>
2026-02-04 16:54:38 -0300

    feat: implementa função de adicionar solicitação

---
+++ sistema.py
@@ -0,0 +1,19 @@
+# Projeto pré carnaval.
+# Foco deste projeto é a criação de um Sistema de Atendimento por Prioridade.
+
+fila = []
+contador_ordem = 0
+solicitacao = {}
+
+def adicionar_solicitacao(fila, cliente, problema, nivel_de_prioridade, ordem_de_chegada):
+    solicitacao = {
+        "cliente": cliente,
+        "problema": problema,
+        "nivel_de_prioridade": nivel_de_prioridade,
+        "ordem_de_chegada": ordem_de_chegada
+        }
+    
+    fila.append(solicitacao)
+    contador_ordem += 1
+
+
