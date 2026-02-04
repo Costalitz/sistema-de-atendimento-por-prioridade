c77a4885469cfd6f25d74ce0d8471560b496eab6
João Costa <PedroM_187@hotmail.com>
2026-02-04 17:15:08 -0300

    feat: implementa função criar_solicitacao.

--- sistema.py
+++ sistema.py
@@ -5,6 +5,23 @@ fila = []
 contador_ordem = 0
 solicitacao = {}
 
+def criar_solicitacao(cliente, problema, ordem_de_chegada):
+    nivel_de_prioridade = {
+        "software": 3,
+        "hardware": 2,
+        "duvida": 1
+    }
+
+    solicitacao = {
+        "cliente": cliente,
+        "problema": problema,
+        "nivel_de_prioridade": nivel_de_prioridade[problema],
+        "ordem_de_chegada": ordem_de_chegada
+    }
+
+    return solicitacao
+
+
 def adicionar_solicitacao(fila, cliente, problema, nivel_de_prioridade, ordem_de_chegada):
     solicitacao = {
         "cliente": cliente,
